209c7281133642000c854b836af9324477b2c7a6
liutao <1342436308@qq.com>
2019-12-27 14:49:40 +0800

    2.6封装组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScrollableTabComponent } from './components/scrollable-tab/scrollable-tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollableTabComponent
   ],
   imports: [
     BrowserModule
