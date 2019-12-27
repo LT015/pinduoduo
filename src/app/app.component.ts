ef5235709b9ca7da7316b1324f339610d060af9c
liutao <1342436308@qq.com>
2019-12-27 13:28:52 +0800

    2.4-让-TopBar-支持滚动

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
 import { Component } from '@angular/core';
 
+interface TopMenu {
+    title: string;
+    link: string;
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pinduoduo';
+    /**
+     * 声明类型的好处是，可以及时的提示错误，可以试着改变下面的 title 或 link 的值类型
+     * 看 VS Code 中的提示
+     */
+    menus: TopMenu[] = [
+        {
+            title: '热门',
+            link: ''
+        },
+        {
+            title: '男装',
+            link: ''
+        },
+        {
+            title: '百货',
+            link: ''
+        },
+        {
+            title: '运动',
+            link: ''
+        },
+        {
+            title: '手机',
+            link: ''
+        },
+        {
+            title: '家纺',
+            link: ''
+        },
+        {
+            title: '食品',
+            link: ''
+        },
+        {
+            title: '电器',
+            link: ''
+        },
+        {
+            title: '鞋包',
+            link: ''
+        },
+        {
+            title: '汽车',
+            link: ''
+        },
+        {
+            title: '水果',
+            link: ''
+        },
+        {
+            title: '电脑',
+            link: ''
+        },
+        {
+            title: '内衣',
+            link: ''
+        },
+        {
+            title: '家装',
+            link: ''
+        },
+        {
+            title: '母婴',
+            link: ''
+        },
+        {
+            title: '美妆',
+            link: ''
+        },
+        {
+            title: '家具',
+            link: ''
+        }
+    ];
 }
