86bb71c22cde6620e0ee26d1a9b588ac8e7f3f39
liutao <1342436308@qq.com>
2019-12-27 16:10:52 +0800

    2.7组件的数据输入输出

--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
-interface TopMenu {
+export interface TopMenu {
     title: string;
     link: string;
 }
@@ -18,76 +18,10 @@ export class ScrollableTabComponent implements OnInit {
      * 声明类型的好处是，可以及时的提示错误，可以试着改变下面的 title 或 link 的值类型
      * 看 VS Code 中的提示
      */
-    menus: TopMenu[] = [
-        {
-            title: '热门',
-            link: ''
-        },
-        {
-            title: '男装',
-            link: ''
-        },
-        {
-            title: '百货',
-            link: ''
-        },
-        {
-            title: '运动',
-            link: ''
-        },
-        {
-            title: '手机',
-            link: ''
-        },
-        {
-            title: '家纺',
-            link: ''
-        },
-        {
-            title: '食品',
-            link: ''
-        },
-        {
-            title: '电器',
-            link: ''
-        },
-        {
-            title: '鞋包',
-            link: ''
-        },
-        {
-            title: '汽车',
-            link: ''
-        },
-        {
-            title: '水果',
-            link: ''
-        },
-        {
-            title: '电脑',
-            link: ''
-        },
-        {
-            title: '内衣',
-            link: ''
-        },
-        {
-            title: '家装',
-            link: ''
-        },
-        {
-            title: '母婴',
-            link: ''
-        },
-        {
-            title: '美妆',
-            link: ''
-        },
-        {
-            title: '家具',
-            link: ''
-        }
-    ];
+    @Input()
+    menus: TopMenu[] ;
+    @Output()
+    tabSelected = new EventEmitter();
 
     constructor() { }
 
@@ -98,5 +32,6 @@ export class ScrollableTabComponent implements OnInit {
 
     handleSelection(index: number) {
         this.selectedIndex = index;
+        this.tabSelected.emit(this.menus[this.selectedIndex]);
     }
 }
