1820b63336c92ae8b21bf032fb4a2154b067f602
liutao <1342436308@qq.com>
2020-01-02 15:09:58 +0800

    依赖注入

--- src/app/home/components/home-container/home-container.component.ts
+++ src/app/home/components/home-container/home-container.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TopMenu } from 'src/app/shared/components';
 import {Router} from '@angular/router';
+import {HomeService} from '../../services';
+import {serialize} from '@angular/compiler/src/i18n/serializers/xml_helper';
 
 @Component({
   selector: 'app-home-container',
@@ -9,101 +11,14 @@ import {Router} from '@angular/router';
 })
 export class HomeContainerComponent implements OnInit {
 
-    constructor(private router: Router) { }
-
-    topMenus: TopMenu[] = [
-        {
-            title: '热门',
-            link: 'hot',
-            id: 1
-        },
-        {
-            id: 2,
-            title: '男装',
-            link: 'men'
-        },
-        {
-            id: 3,
-            title: '百货',
-            link: 'department'
-        },
-        {
-            id: 4,
-            title: '运动',
-            link: 'sports'
-        },
-        {
-            id: 5,
-            title: '手机',
-            link: 'mobile'
-        },
-        {
-            id: 6,
-            title: '家纺',
-            link: 'textile'
-        },
-        {
-            id: 7,
-            title: '食品',
-            link: 'food'
-        },
-        {
-            id: 8,
-            title: '电器',
-            link: 'appliance'
-        },
-        {
-            id: 9,
-            title: '鞋包',
-            link: 'shoes'
-        },
-        {
-            id: 10,
-            title: '汽车',
-            link: 'cars'
-        },
-        {
-            id: 11,
-            title: '水果',
-            link: 'fruits'
-        },
-        {
-            id: 12,
-            title: '电脑',
-            link: 'computers'
-        },
-        {
-            id: 13,
-            title: '内衣',
-            link: 'underwears'
-        },
-        {
-            id: 14,
-            title: '家装',
-            link: 'home'
-        },
-        {
-            id: 15,
-            title: '母婴',
-            link: 'baby'
-        },
-        {
-            id: 16,
-            title: '美妆',
-            link: 'makeup'
-        },
-        {
-            id: 17,
-            title: '家具',
-            link: 'furnitures'
-        }
-    ];
-
+    constructor(private router: Router, private service: HomeService) { }
+    topMenus: TopMenu[] = [];
     ngOnInit() {
-    }
 
+    }
 
     handleTabSelected(topMenu: TopMenu) {
         this.router.navigate(['home', topMenu.link]);
+        this.topMenus = this.service.getTabs();
     }
 }
--- src/app/home/components/home-grand/home-grand.component.ts
+++ src/app/home/components/home-grand/home-grand.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, InjectionToken, Injector, OnInit} from '@angular/core';
+
+@Injectable()
+class Product {
+    constructor(
+        private name: string,
+        private model: string,
+        private color: string,
+        private price: number,
+        private type: string
+    ) {}
+}
+
+@Injectable()
+class PurchaseOrder {
+    constructor(
+        private product: Product,
+        private amount: number,
+        private buyer: string
+    ) {}
+
+    public get getProduct(): Product {
+        return this.product;
+    }
+}
 
 @Component({
   selector: 'app-home-grand',
@@ -16,6 +40,31 @@ export class HomeGrandComponent implements OnInit {
     ngOnInit() {
         this.price = 15313.366;
         this.date = this.minusMonths(new Date(), 15);
+        const product = new Product('大米手机', 'DM-9', '黑色', 2999, '全网通');
+        const purchaseOrder = new PurchaseOrder(product, 10, '张三');
+        console.log('订单', purchaseOrder.getProduct);
+        const token = new InjectionToken<string>('config');
+        const injector = Injector.create({
+            providers: [
+                {
+                    provide: Product,
+                    useFactory: () => {
+                        return new Product('大米手机', 'DM-9', '黑色', 2999, '全网通');
+                    },
+                    deps: []
+                },
+                {
+                    provide: PurchaseOrder,
+                    deps: [Product]
+                },
+                {
+                    provide: token,
+                    useValue: { baseUrl: 'http://local.dev' }
+                }
+            ]
+        });
+        console.log(injector.get(PurchaseOrder).getProduct);
+        console.log(injector.get(token));
     }
     minusDays(date: Date, days: number) {
         const result = new Date(date);
